89510bea21fbb22eb21eb19a6f9f0ff2005e0577
Rohit <rohitrmr07@gmail.com>
2026-01-15 11:10:03 +0530

    Frontend and Backend of Landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Accesschoice from "./pages/Access/Accesschoice";
+import DeliveryAccess from "./pages/Access/DeliveryAccess";
+import PickupAccess from "./pages/Access/PickupAccess";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// If you already have a Layout component, wrap Routes with it
+export default function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/access" element={<Accesschoice />} />
+        <Route path="/access/delivery" element={<DeliveryAccess />} />
+        <Route path="/access/pickup" element={<PickupAccess />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
