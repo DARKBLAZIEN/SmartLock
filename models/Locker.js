3562b04811f069175c57bb69a76b319687aa6116
Rohit <rohitrmr07@gmail.com>
2026-01-15 22:41:43 +0530

    New Frontend and Backend of Landing Page

--- models/Locker.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const lockerSchema = new mongoose.Schema({
-  lockerId: { type: String, required: true, unique: true },
-  isFree: { type: Boolean, default: true }
-},{ collection: 'lockers' });
-
-module.exports = mongoose.model("Locker", lockerSchema);
--- models/PickupLog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const pickupLogSchema = new mongoose.Schema({
-  apartmentId: { type: String, required: true },
-  lockerId: { type: String, required: true },
-  pickupTime: { type: Date, default: Date.now }
-},{ collection: 'pickup_logs' });
-
-module.exports = mongoose.model("PickupLog", pickupLogSchema);
